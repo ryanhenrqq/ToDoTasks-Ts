a0603048e4bdef0aab8bbce3b43d146fa9c720a7
ryanhenrqq <ryanh8116@gmail.com>
2026-09-25 10:44:20 -0300

    feat/on-building: added notification grant logic

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,32 @@ import { TasksView, CleanView } from './components/views/taskview'
 
 function App() {
   const [noteList, setNoteList] = useState<Note[]>([])
+  const [allowNots, setAllowNots] = useState(false)
+  const [requestingNots, setRequestingNots] = useState(false)
   useEffect(() => {
     const checker = JSON.parse(localStorage.getItem('notes-storage') ?? '[]');
     setNoteList(checker)
+    if (Notification.permission !== 'granted'){
+      setAllowNots(false)
+    } else {
+      setAllowNots(true)
+    }
   }, []);
   const updateNotes = (newNotes: Note[]) => {
-    console.log(newNotes)
-    console.log(noteList)
+    if (!requestingNots){
+      setRequestingNots(true)
+      givePerms()
+    } else {
+      if (Notification.permission !== 'granted'){
+        setAllowNots(false)
+        setRequestingNots(true)
+      } else {
+        setAllowNots(true)
+        setRequestingNots(false)
+      }
+    }
+    console.log(newNotes)  //debug
+    console.log(noteList)  //debug
     setNoteList(newNotes);
     localStorage.setItem('notes-storage', JSON.stringify(newNotes));
   };
@@ -21,8 +40,25 @@ function App() {
     <>
       <Header note={noteList} onChange={updateNotes} />
       {noteList.length == 0 ? <CleanView /> : <TasksView note={noteList} onChangeNotes={updateNotes} />}
+      <div className="center-ver">
+        {!allowNots ? <b>Notificações bloqueadas</b>:null}
+      </div>
     </>
   )
+}
+
+async function givePerms(): Promise<boolean> {
+  if (!("Notification" in window)){
+    console.error('Notificações Indisponíveis')
+  }
+  if (Notification.permission === 'granted'){
+    return true
+  }
+  if (Notification.permission !== 'denied'){
+    const perm = await Notification.requestPermission()
+    return perm === 'granted'
+  }
+  return false
 }
 
 export default App
